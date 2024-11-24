a42725786baf19d7cea8861c8a80180a4a00b697
Marcin Lach <marcin.lach.90@gmail.com>
2024-11-24 12:34:27 +0100

    Remove weatherforcast and create GetBestStoriesUseCase

---
+++ src/santa.api/GetBestStoriesUseCase/GetBestStory.cs
@@ -0,0 +1,31 @@
+﻿namespace santa.api.GetBestStoriesUseCase;
+
+public static class GetBestStories
+{
+    private record BestStory(string Title, string Uri, string PostedBy, DateTime Time, int Score, int CommentCount);
+
+    public static class Endpoint
+    {
+        public static void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/beststories", Handler)
+                .WithName("GetBestStories");
+        }
+    }
+
+    private static IResult Handler(HttpContext context)
+    {
+        var bestStories = Enumerable.Range(1, 5).Select(index =>
+                new BestStory
+                (
+                    $"Title{index}",
+                    "Uri",
+                    "PostedBy",
+                    DateTime.Now.AddDays(index),
+                    Random.Shared.Next(20, 55),
+                    Random.Shared.Next(20, 55)
+                ))
+            .ToArray();
+        return Results.Ok(bestStories);
+    }
+}
