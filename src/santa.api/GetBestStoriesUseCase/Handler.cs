9187c7663e316b0ddad5a51a679a1c09d4d2a2f2
Marcin Lach <marcin.lach.90@gmail.com>
2024-11-24 14:05:05 +0100

    Fetch BestStoriesIds from HackerRank API

--- src/santa.api/GetBestStoriesUseCase/Handler.cs
+++ src/santa.api/GetBestStoriesUseCase/Handler.cs
@@ -6,11 +6,13 @@ public static partial class GetBestStories
 {
     private static async Task<IResult> Handler(int numberOfStories, HttpContext context, IHackerRankApi hackerRankApi)
     {
+        var bestStoriesIds = await hackerRankApi.GetBestStoriesIds();
+        
         var bestStories = Enumerable.Range(1, numberOfStories).Select(index =>
                 new BestStory
                 (
                     $"Title{index}",
-                    "HR stories count",
+                    $"HR stories count: {bestStoriesIds.Length}",
                     "PostedBy",
                     DateTime.Now.AddDays(index),
                     Random.Shared.Next(20, 55),
